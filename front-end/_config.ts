5a5d3c2720642b9d64d1b7125c105276fe22b60b
urataps <alexeluca.spataru@gmail.com>
2024-01-19 15:59:45 +0200

    feat: add front-end

---
+++ front-end/_config.ts
@@ -0,0 +1,16 @@
+const config = {
+  metadata: {
+    title: 'DeFi Builder | Oraichain Hackathon',
+    // eslint-disable-next-line quotes
+    description: "DeFi Builder's project for Oraichain Hackathon",
+    keywords:
+      'react.js, react.js-template, react.js-boilerplate, typescript, shadcn/ui, tailwindcss, vite, swc, eslint, prettier, husky, commitlint, lint-staged'
+  },
+  server: {
+    host: 'localhost',
+    port: 3000
+  }
+};
+
+export default config;
+
---
+++ front-end/src/interfaces/template.ts
@@ -0,0 +1,7 @@
+/* eslint-disable semi */
+
+export default interface ITemplate {
+  name: string;
+  isActive: boolean;
+  iconURL: string;
+}
